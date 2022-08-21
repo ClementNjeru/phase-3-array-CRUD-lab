f83165bbfc056a9d15d9d1b67d497df5d3c0fd2b
ClementNjeru <njeruclement36@gmail.com>
2022-08-21 11:28:21 +0300

    commit tree c5928d593b756ec4978c84746494b9448b4a9548

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+fruit =  ["mango", "apple", "orange", "passion"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(fruit, banana)
+  fruit << banana
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(fruit, banana)
+  fruit.unshift (banana)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(fruit)
+  fruit.pop 
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(fruit)
+  fruit.shift 
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(fruit, index_number)
+  fruit [index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(fruit)
+  fruit.first  
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(fruit)
+    fruit.last
+
 end
